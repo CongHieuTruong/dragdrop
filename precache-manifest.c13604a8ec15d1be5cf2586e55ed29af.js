1b3f2ec68c81fb30bac4fd089c1f14f1f4ca8ca0
Hieu Truong <hieu.tnxd.bk@gmail.com>
2021-06-20 19:13:25 +0700

    Updates

---
+++ precache-manifest.c13604a8ec15d1be5cf2586e55ed29af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3509fd8ba8f908cf2f7",
+    "url": "/static/css/main.5046f5e0.chunk.css"
+  },
+  {
+    "revision": "c3509fd8ba8f908cf2f7",
+    "url": "/static/js/main.b5210815.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3ef57fb0bb149e1c0c8b",
+    "url": "/static/css/2.2a136979.chunk.css"
+  },
+  {
+    "revision": "3ef57fb0bb149e1c0c8b",
+    "url": "/static/js/2.8842e11e.chunk.js"
+  },
+  {
+    "revision": "efd541584666970484bf50c5b4346dfa",
+    "url": "/index.html"
+  }
+];
